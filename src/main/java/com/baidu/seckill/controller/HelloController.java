1b760e8ed334a326ae759f80182df708fdbff4c6
Administrator <zhangchangfu1987@163.com>
2019-12-15 19:13:02 +0800

    增加自定义mapper

--- src/main/java/com/baidu/seckill/controller/HelloController.java
+++ src/main/java/com/baidu/seckill/controller/HelloController.java
@@ -1,6 +1,6 @@
 package com.baidu.seckill.controller;
 
-import com.baidu.seckill.mapper.SecKillMapper;
+import com.baidu.seckill.dao.SecKillMapper;
 import com.baidu.seckill.pojo.SecKill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
